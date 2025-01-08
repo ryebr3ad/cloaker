039e8c78b6110b1d3760aa42ad30cc424470ec53
= <rprauschenberg@gmail.com>
2025-01-07 22:46:33 -0500

    Move content script to load initially, then set an interval function to excute the initial load (first iteration -- redo this in future)

--- popup/cloaker_popup.js
+++ popup/cloaker_popup.js
@@ -8,16 +8,4 @@ function listenForCloakingStrategy() {
   });
 }
 
-//The intent here is to trigger the window into accessing storage to get the list of IDs it needs to block out of the gate
-function loadInitialBlockedElements() {
-  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-    browser.tabs.sendMessage(tabs[0].id, {
-      command: "load",
-    });
-  });
-}
-
-browser.tabs
-  .executeScript({ file: "/content_scripts/cloaker.js" })
-  .then(loadInitialBlockedElements)
-  .then(listenForCloakingStrategy);
+browser.tabs.getCurrent().then(listenForCloakingStrategy);
